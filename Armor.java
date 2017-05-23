44d9694ceb2d0400780b80f70c75cd4c82d81a59
macuna43278 <macuna@stuy.edu>
2017-05-23 10:04:49 -0400

    Added BST & Armor & TreeNode classes

---
+++ Armor.java
@@ -0,0 +1,21 @@
+public class Armor{
+    
+    public BST _armor;
+    private int level;
+
+    //constructor
+    public Armor(){
+	BST _armor = new BST();
+    }
+    
+    //overloaded constructor (mayb?)
+    public Armor(int lvl){
+	//Fills each level with nodes each containing the number of the level.
+        for(int temp = 0; temp <= lvl; temp++){
+	    for(int count = (int)Math.pow(2, lvl); count >= 0; count--){
+		_armor.insert(lvl);
+	    }
+	}
+    }
+    
+}
--- Ship.java
+++ Ship.java
@@ -1,11 +1,17 @@
-public class ship{
+public class Ship{
 
     //Instance vars
     private int attackStat;
     private int armorStat;
     private int speedStat;
     private boolean isLives;
+    public Ship boat;
+    private Armor armor;
 
+    //constructor
+    public Ship(){
+	Ship boat = new Ship();
+    }
 
     //Accessors
     public boolean alive(){
@@ -17,9 +23,24 @@ public class ship{
     public int getArmor(){
 	return armorStat;
     }
-    public int speedStat(){
+    public int getSpeed(){
 	return speedStat;
     }
-
     
+    //modifiers
+    public int setAttack(int newAttack){
+	int temp = boat.getAttack();
+	attackStat = newAttack;
+	return temp;
+    }
+    public int setArmor(int newArmor){
+	int temp = boat.getArmor();
+	armorStat = newArmor;
+	return temp;
+    }
+    public int setSpeed(int newSpeed){
+	int temp = boat.getSpeed();
+	speedStat = newSpeed;
+	return temp;
+    }
 }
