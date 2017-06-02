882fab2e0bf4505fa4608607c5064a66f5696d50
Alessandro Cartegni <acartegni@stuy.edu>
2017-06-02 10:13:28 -0400

    APQ

---
+++ ArrayPriorityQueue.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+    
+public class ArrayPriorityQueue<T> implements PriorityQueue<T> {
+    
+    ArrayList<T> PQueue;
+    
+    public ArrayPriorityQueue(){
+	PQueue = new ArrayList<T>();
+    }
+
+    public void add (T x){
+	if (isEmpty() ||((Comparable)x).compareTo((Comparable)PQueue.get(PQueue.size() - 1)) < 0){
+	    PQueue.add(x);
+	}
+	else{
+	    for (int i = 0; i < PQueue.size(); i++){
+		if (((Comparable)x).compareTo((Comparable)PQueue.get(i)) >= 0){
+		    PQueue.add(i, x);
+		    break;
+		}
+	    }
+	}
+    }
+
+    public boolean isEmpty(){
+	return PQueue.size() == 0;
+    }
+
+    public T peekMin(){
+	return PQueue.get(PQueue.size() - 1);
+    }
+
+    public T removeMin(){
+	return PQueue.remove(PQueue.size() - 1);
+	
+    }
+
+    public String toString(){
+	String retStr = "";
+	for (int x = 0; x < PQueue.size(); x++){
+	    retStr += PQueue.get(x) + ", ";
+	}
+	return retStr;
+    }	
+}
