92748ab0a915f9bd0f4e01ce6c12fd61ce6da412
maria43278 <macuna@stuy.edu>
2017-06-06 22:05:01 -0400

    comments!

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -2,43 +2,46 @@ import java.util.ArrayList;
 
 public class ArrayPriorityQueue<T> {
 
-  ArrayList<T> PQueue;
+    ArrayList<T> PQueue;
 
-  public ArrayPriorityQueue() {
-    PQueue = new ArrayList<T>();
-  }
-
-  public void add (T x) {
-    if (isEmpty() || ((Comparable)x).compareTo((Comparable)PQueue.get(0)) > 0) {
-      PQueue.add(0,x);
-    } else {
-      for (int i = 0; i < PQueue.size(); i++) {
-        if (((Comparable)x).compareTo((Comparable)PQueue.get(i)) >= 0) {
-          PQueue.add(i, x);
-          return;
-        }
-      }
-      PQueue.add(PQueue.size(), x);
+    //constructor
+    public ArrayPriorityQueue() {
+	PQueue = new ArrayList<T>();
     }
-  }
 
-  public boolean isEmpty() {
-    return PQueue.size() == 0;
-  }
-
-  public T peekMax() {
-    return PQueue.get(0);
-  }
-
-  public T removeMax() {
-    return PQueue.remove(0);
-  }
-
-  public String toString() {
-    String retStr = "";
-    for (int x = 0; x < PQueue.size(); x++) {
-      retStr += PQueue.get(x) + ", ";
+    //add method, adds enqueues object based on priority (greater priority --> higher priority)
+    public void add (T x) {
+	if (isEmpty() || ((Comparable)x).compareTo((Comparable)PQueue.get(0)) > 0) {
+	    PQueue.add(0,x);
+	}
+	else {
+	    for (int i = 0; i < PQueue.size(); i++) {
+		if (((Comparable)x).compareTo((Comparable)PQueue.get(i)) >= 0) {
+		    PQueue.add(i, x);
+		    return;
+		}
+	    }
+	    PQueue.add(PQueue.size(), x);
+	}
     }
-    return retStr;
-  }
+
+    public boolean isEmpty() {
+	return PQueue.size() == 0;
+    }
+
+    public T peekMax() {
+	return PQueue.get(0);
+    }
+
+    public T removeMax() {
+	return PQueue.remove(0);
+    }
+
+    public String toString() {
+	String retStr = "";
+	for (int x = 0; x < PQueue.size(); x++) {
+	    retStr += PQueue.get(x) + ", ";
+	}
+	return retStr;
+    }
 }
